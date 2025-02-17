8b8df07544600f3635d47f7bcd4c7c4c5108297f
Adrien444e <adrien.gougeon@hotmail.com>
2025-02-17 13:34:54 +0000

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*
+* main - Printz the size of various types on the computer it is compiled and run on
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	printf("Size of a char: (1));
+	printf("Size of an int: (4));
+	printf("Size Size of a long int: (4));
+	printf("Size of a long long int: (8));
+	printf("Size of a float: (4));
+
+	retnrn (0);
+}
