e61616629ccc98c97d52b703829e27da18230a0b
Adrien444e <adrien.gougeon@hotmail.com>
2025-02-18 08:24:11 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,4 +3,17 @@
 #include <stdio.h>
 
 /**
-* main - 
+* main - prints "more headers goes there"
+*
+* Returne: Always 0.
+*/
+int main(void)
+{
+	int (n);
+
+srand'time(0));
+n = rand () -RAND_MAX / (2);
+printf("%d \n", (x));
+
+return (0);
+}
