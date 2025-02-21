5901ac45a3a83efddac616e8a09b4a26d78a13f6
Adrien444e <adrien.gougeon@hotmail.com>
2025-02-21 13:28:04 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: variable a
+ * @b: variable b
+ * Return: c (Success)
+*/
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
