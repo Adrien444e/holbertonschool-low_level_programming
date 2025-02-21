0d45fdfbb9aac93352f0e441f420d703ab41f573
Adrien444e <adrien.gougeon@hotmail.com>
2025-02-21 08:26:16 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @c: variable c
+ * Return: c (Success) Return: -c (Fail)
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+
+		return (c);
+
+	else
+
+		return (-c);
+}
