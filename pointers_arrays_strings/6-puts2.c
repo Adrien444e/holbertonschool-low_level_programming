854a1b150b0311f803cd54029d8dd5e843dea859
Adrien444e <adrien.gougeon@hotmail.com>
2025-02-27 13:46:50 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: variable str
+ * 
+ */
+
+void puts2(char *str)
+{
+	int doubl;
+
+	for (doubl = 0; str[doubl] != '\0'; doubl += 2)
+	{
+		_putchar(str[doubl]);
+	}
+	_putchar('\n');
+}
